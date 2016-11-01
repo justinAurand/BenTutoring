e5a06542a4e140c1c3f2dabcf9b02704415c5a56
Justin Aurand <justin.aurand@swiftcapital.com>
2016-10-31 22:44:11 -0400

    Completed validation of first email

--- ValidatePassword/ValidatePassword.java
+++ ValidatePassword/ValidatePassword.java
@@ -12,9 +12,16 @@ public class ValidatePassword {
 
             if (!IsLongEnough(password))
                 continue;
-            if (!HasNoSpaces(password))
+            if (HasASpace(password))
                 continue;
-            // Check contains a letter, number, and symbol
+            if (!HasLetterNumberAndSymbol(password))
+                continue;
+            if (StartsWithExclamation(password))
+                continue;
+            if (EndsWithHyphen(password))
+                continue;
+            if (HasThreeConsecutiveCharacters(password))
+                continue;
 
             valid = true;
         }
@@ -28,18 +35,67 @@ public class ValidatePassword {
         return true;
     }
 
-    private static boolean HasNoSpaces(String pw) {
+    private static boolean HasASpace(String pw) {
         if (pw.contains(" ")) {
             System.out.println("Password cannot contain spaces.");
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean HasLetterNumberAndSymbol(String pw) {
+        char[] letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz".toCharArray();
+        char[] numbers = "0123456789".toCharArray();
+        char[] symbols = "~!@#$%^&*()_+{}|<>?".toCharArray();
+
+        boolean hasLetter = HasIntersetion(pw, letters);
+        boolean hasNumber = HasIntersetion(pw, numbers);
+        boolean hasSymbol = HasIntersetion(pw, symbols);
+
+        if (!hasLetter || !hasNumber || !hasSymbol) {
+            System.out.println("Password must contain a letter, number, and symbol.");
             return false;
         }
         return true;
     }
 
-    private static boolean HasALetterNumberAndSymbol(String pw) {
-        char[] letters = "abcdefghijklmnopqrstuvwxyz".toCharArray();
-        char[] numbers = "0123456789".toCharArray();
-        char[] symbols = "~!@#$%^&*()_+{}|<>?".toCharArray();
+    private static boolean HasIntersetion(String pw, char[] values) {
+        for (int i = 0; i < values.length; i++) {
+            String value = Character.toString(values[i]);
+            if (pw.contains(value))
+                return true;
+        }
+        return false;
+    }
+
+    private static boolean StartsWithExclamation(String pw) {
+        if (pw.startsWith("!")) {
+            System.out.println("Password cannot start with an exclamation point.");
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean EndsWithHyphen(String pw) {
+        if (pw.endsWith("-")) {
+            System.out.println("Password cannot end with a hyphen.");
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean HasThreeConsecutiveCharacters(String pw) {
+        char[] pass = pw.toCharArray();
+        for (int i = 0; i < pass.length; i++)
+        {
+            if (i + 2 >= pass.length)
+                break;
+
+            if (pass[i] == pass[i+1] && pass[i+1] == pass[i+2]) {
+                System.out.println("Password cannot contain three consecutive characters.");
+                return true;
+            }
+        }
         return false;
     }
 }
