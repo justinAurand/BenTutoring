7afef2353feeead702258d4812f4c454db42af88
Justin Aurand <justin.aurand@swiftcapital.com>
2016-11-27 20:54:10 -0500

    Added listener that can count button clicks

--- RemoteControl/RemoteControl.java
+++ RemoteControl/RemoteControl.java
@@ -13,15 +13,15 @@ public class RemoteControl implements ActionListener {
     }
 
     public void actionPerformed(ActionEvent e) {
-        JButton source = (JButton)e.getSource();
-        printButtonIdentity(source);
+        JButton button = (JButton)e.getSource();
+        printButtonIdentity(button);
     }
 
     public static void main(String[] args) {
         RemoteControl remote = new RemoteControl();
     }
 
-    private JButton getButton(String text, Font font, Color color) {
+    private JButton getButtonWithListener(String text, Font font, Color color) {
         JButton button = new JButton();
         button.addActionListener(this);
         if (text != null && text.length() > 0)
@@ -35,45 +35,58 @@ public class RemoteControl implements ActionListener {
 
     private JPanel getChannelAndVolumePanel() {
         JPanel panel = new JPanel(new GridLayout (5,3));
+        VolumeListener listener = new VolumeListener();
+
+        JButton volumeUpButton = new JButton("Vol. +");
+        JButton volumeDownButton = new JButton("Vol. -");
+        volumeUpButton.addActionListener(listener);
+        volumeDownButton.addActionListener(listener);
+
         for (int i=1; i<10; i++)
-            panel.add(getButton(Integer.toString(i), null, null));
-        panel.add(getButton("Ch. -", null, null));
-        panel.add(getButton("0", null, null));
-        panel.add(getButton("Ch. +", null, null));
-        panel.add(getButton("Vol. +", null, null));
-        panel.add(getButton("Mute", null, null));
-        panel.add(getButton("Vol. -", null, null));
+            panel.add(getButtonWithListener(Integer.toString(i), null, null));
+        panel.add(getButtonWithListener("Ch. -", null, null));
+        panel.add(getButtonWithListener("0", null, null));
+        panel.add(getButtonWithListener("Ch. +", null, null));
+        panel.add(volumeUpButton);
+        panel.add(getButtonWithListener("Mute", null, null));
+        panel.add(volumeDownButton);
+
         return panel;
     }
 
     private JPanel getDirectionPanel() {
         JPanel panel = new JPanel(new GridLayout (2,3));
-        panel.add(getImageButton("play.png"));
-        panel.add(getImageButton("pause.png"));
-        panel.add(getImageButton("stop.png"));
-        panel.add(getImageButton("rewind.png"));
+
+        panel.add(getImageButtonWithListener("play.png"));
+        panel.add(getImageButtonWithListener("pause.png"));
+        panel.add(getImageButtonWithListener("stop.png"));
+        panel.add(getImageButtonWithListener("rewind.png"));
         panel.add(new JButton());
-        panel.add(getImageButton("fast-forward.png"));
+        panel.add(getImageButtonWithListener("fast-forward.png"));
+
         return panel;
     }
 
-    private JButton getImageButton(String image) {
+    private JButton getImageButtonWithListener(String image) {
         ImageIcon icon = new ImageIcon(image);
         JButton button = new JButton(icon);
         button.setName(image.substring(0, image.lastIndexOf('.')));
         button.addActionListener(this);
+
         return button;
     }
 
     private JPanel getPowerAndInputPanel() {
         Font arialBold = new Font("Arial", Font.BOLD, 24);
         JPanel panel = new JPanel(new GridLayout (3,2));
-        panel.add(getButton("Off", arialBold, Color.red));
-        panel.add(getButton("On", arialBold, Color.red));
-        panel.add(getButton("DVD", arialBold, null));
-        panel.add(getButton("Aux", arialBold, null));
-        panel.add(getButton("Cable", arialBold, null));
-        panel.add(getButton("Menu", arialBold, null));
+
+        panel.add(getButtonWithListener("Off", arialBold, Color.red));
+        panel.add(getButtonWithListener("On", arialBold, Color.red));
+        panel.add(getButtonWithListener("DVD", arialBold, null));
+        panel.add(getButtonWithListener("Aux", arialBold, null));
+        panel.add(getButtonWithListener("Cable", arialBold, null));
+        panel.add(getButtonWithListener("Menu", arialBold, null));
+
         return panel;
     }
 
@@ -101,5 +114,26 @@ public class RemoteControl implements ActionListener {
         }
 
         System.out.println("Undefined");
+    }
+
+    private class VolumeListener implements ActionListener {
+        private int volume;
+
+        private VolumeListener() {
+            volume = 0;
+        }
+
+        public int getVolume() { return volume; }
+
+        public void actionPerformed(ActionEvent e) {
+            JButton button = (JButton)e.getSource();
+            String text = button.getText();
+            if (text.contains("+"))
+                volume++;
+            if (text.contains("-"))
+                volume--;
+
+            System.out.println("Volume: " + Integer.toString(volume));
+        }
     }
 }
